0014f8ab54f50509038c2430380a2e5bde0b5541
vahid ghadiri <whydrf@gmail.com>
2026-07-18 12:36:51 +0330

    chore(components/button): Expose all button related module and types

--- apps/package/src/components/_abstractions/index.ts
+++ apps/package/src/components/_abstractions/index.ts
@@ -1,2 +1,9 @@
+export type {
+  AbstractedButtonProps,
+  UseButtonOptions,
+  LoadingPlacement,
+  UseButtonResult,
+  ButtonSlots,
+} from "./abstracted-button/button.types";
 export { default as AbstractedButton } from "./abstracted-button";
-export type { AbstractedButtonProps } from "./abstracted-button";
+export { useButton } from "./abstracted-button/hooks/use-button";
